8dd554dc0490a4633c3160248720775342f6dede
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-07-18 17:57:27 +0900

    fixed dockre_run.sh

--- docker_run.sh
+++ docker_run.sh
@@ -3,7 +3,7 @@ export GIT_ROOT=$(cd $(dirname $0)/.. ; pwd)
 sudo docker run -it --rm --net=host --runtime nvidia -e DISPLAY=$DISPLAY \
 	--device /dev/bus/usb \
 	--device /dev/video0:/dev/video0:mwr \
-	-v v ${GIT_ROOT}/Depth4ToM-docker/weights:/root/Depth4ToM-code/weights \
+	-v ${GIT_ROOT}/Depth4ToM-docker/weights:/root/Depth4ToM-code/weights \
 	-v /tmp/.X11-unix/:/tmp/.X11-unix \
 	depth4tom:100
  
